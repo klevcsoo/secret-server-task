44f16dbcd978aa3954846e06c39f1940ff20868f
Keve Lajsz <kevelajsz@icloud.com>
2022-07-06 19:40:31 +0200

    Added proper console logging to server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,20 +2,48 @@ import express from "express";
 import cors from "cors";
 import {EXPRESS_PORT} from "./config";
 
+function logWithTime(type: "info" | "error", body: any) {
+    const timestamp = (() => {
+        const raw = new Date();
+        const d = raw.toDateString();
+        const t = raw.toTimeString().substring(
+            0, raw.toTimeString().indexOf(" ")
+        );
+        return `${d} ${t}`;
+    })();
+    switch (type) {
+        case "info": {
+            console.log(`[${timestamp}] INFO:`, body);
+            break;
+        }
+        case "error": {
+            console.error(`[${timestamp}] ERROR:`, body);
+            break;
+        }
+        default: {
+            console.log(`[${timestamp}]`, body);
+            break;
+        }
+    }
+}
+
 const app = express();
 app.use(cors());
 
 app.listen(EXPRESS_PORT, () => {
-    console.log(`Express server listening to requests on port ${EXPRESS_PORT}`);
+    logWithTime(
+        "info",
+        `Express server listening to requests on port ${EXPRESS_PORT}`
+    );
 });
 
 app.post("/v1/secret/", (request, response) => {
-    console.log("[TEST] Creating new secret");
+    logWithTime("info", "Creating new secret");
     response.sendStatus(200);
 });
 
 app.get("/v1/secret/:hash", (request, response) => {
     const hash = request.params["hash"] as string;
-    console.log(`[TEST] Finding secret with hash ${hash}`);
+    logWithTime("info", `Finding secret with hash ${hash}`);
     response.sendStatus(200);
 });
