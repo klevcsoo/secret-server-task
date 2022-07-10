43734d1d1c55ece0162ae9ef7808c8446186ae9c
Keve Lajsz <kevelajsz@icloud.com>
2022-07-10 17:51:03 +0200

    Added MongoDB config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,8 @@
 const EXPRESS_PORT = process.env.EXPRESS_PORT;
-const DEV_ENV = process.env.DEV_ENV;
+const MONGODB_HOST = process.env.MONGODB_HOST;
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_PWD = process.env.MONGODB_PWD;
 
 export {
-    EXPRESS_PORT, DEV_ENV
+    EXPRESS_PORT, MONGODB_HOST, MONGODB_USER, MONGODB_PWD
 };
